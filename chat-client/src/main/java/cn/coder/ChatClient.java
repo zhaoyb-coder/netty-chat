3540c408140e1ff5e4c6f2d09ef03994ac0dabcb
zhaoybc <zhaoybc@yonyou.com>
2024-04-12 14:23:42 +0800

    初始化客户端和服务器

---
+++ chat-client/src/main/java/cn/coder/ChatClient.java
@@ -0,0 +1,14 @@
+package cn.coder;
+
+/**
+ * @author zhaoyubo
+ * @title ChatClient
+ * @description 客户端
+ * @create 2024/4/12 14:20
+ **/
+public class ChatClient {
+
+    public ChatClient() {
+        System.out.println("客户端启动...");
+    }
+}
---
+++ chat-server/src/main/java/cn/coder/ChatServer.java
@@ -0,0 +1,15 @@
+package cn.coder;
+
+/**
+ * @author zhaoyubo
+ * @title ChatServer
+ * @description 服务端
+ * @create 2024/4/12 14:20
+ **/
+public class ChatServer {
+
+    public ChatServer() {
+        System.out.println("服务器启动...");
+    }
+
+}

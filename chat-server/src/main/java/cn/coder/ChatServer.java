9a0b2bb2470cf15e48925c7ebd2278dd18748cb0
zhaoybc <zhaoybc@yonyou.com>
2024-04-26 15:31:10 +0800

    增加Netty服务端启动类

--- chat-server/src/main/java/cn/coder/ChatServer.java
+++ chat-server/src/main/java/cn/coder/ChatServer.java
@@ -1,5 +1,15 @@
 package cn.coder;
 
+import io.netty.bootstrap.ServerBootstrap;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @author zhaoyubo
  * @title ChatServer
@@ -8,8 +18,34 @@ package cn.coder;
  **/
 public class ChatServer {
 
+    private static final int PORT = 10086;
+
     public ChatServer() {
-        System.out.println("服务器启动...");
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+//                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            ChannelFuture f = b.bind(PORT).sync(); // (7)
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+            System.out.println("服务器启动...");
+        }
     }
 
 }

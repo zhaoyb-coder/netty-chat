b9c7f8a82e0ea29e65e6119b5eda5207433e77b7
zhaoybc <zhaoybc@yonyou.com>
2024-04-27 15:19:57 +0800

    增加Netty客户端启动类

--- chat-client/src/main/java/cn/coder/ChatClient.java
+++ chat-client/src/main/java/cn/coder/ChatClient.java
@@ -1,5 +1,20 @@
 package cn.coder;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author zhaoyubo
  * @title ChatClient
@@ -9,6 +24,28 @@ package cn.coder;
 public class ChatClient {
 
     public ChatClient() {
-        System.out.println("客户端启动...");
+        int port = 10086;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap(); // (1)
+            b.group(workerGroup); // (2)
+            b.channel(NioSocketChannel.class); // (3)
+            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
+                }
+            });
+            // Start the client.
+            ChannelFuture f = b.connect("127.0.0.1", port).sync(); // (5)
+            System.out.println("客户端启动...");
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
     }
 }
--- chat-server/src/main/java/cn/coder/ChatServer.java
+++ chat-server/src/main/java/cn/coder/ChatServer.java
@@ -21,30 +21,35 @@ public class ChatServer {
     private static final int PORT = 10086;
 
     public ChatServer() {
-
         EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap(); // (2)
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class) // (3)
+                    // TCP默认开启了 Nagle 算法，该算法的作用是尽可能的发送大数据快，减少网络传输。TCP_NODELAY 参数的作用就是控制是否启用 Nagle 算法。
+                    .childOption(ChannelOption.TCP_NODELAY, true)
                     .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
 //                            ch.pipeline().addLast(new DiscardServerHandler());
                         }
                     })
+                    //表示系统用于临时存放已完成三次握手的请求的队列的最大长度,如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
                     .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    // 是否开启 TCP 底层心跳机制
                     .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
 
+            // 绑定端口，同步等待绑定成功
             ChannelFuture f = b.bind(PORT).sync(); // (7)
+            System.out.println("服务器启动...");
+            // 等待服务端监听端口关闭
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
-            System.out.println("服务器启动...");
         }
     }
 
